a8a140a9455d1fd879a70798a8842b48715992e5
elkanatovey <elkanatovey@gmail.com>
2025-07-07 15:00:48 +0300

    Add Poseidon contract generation and testing scripts for poseidon2

---
+++ test/poseidoncontract2.js
@@ -0,0 +1,90 @@
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const chai = require("chai");
+const poseidonGenContract = require("../src/poseidon_gencontract2.js");
+const poseidon = require("../src/poseidon2.js");
+
+const assert = chai.assert;
+const log = (msg) => { if (process.env.MOCHA_VERBOSE) console.log(msg); };
+
+describe("Poseidon Smart contract test", function () {
+    let testrpc;
+    let web3;
+    let poseidon2;
+    let poseidon4;
+    let poseidon6;
+    let accounts;
+    this.timeout(100000);
+    let C2;
+    let C4;
+    let C6;
+
+    before(async () => {
+        web3 = new Web3(ganache.provider(), null, { transactionConfirmationBlocks: 1 });
+        accounts = await web3.eth.getAccounts();
+    });
+
+    it("Should deploy the contract", async () => {
+        
+        C2 = new web3.eth.Contract(poseidonGenContract.generateABI(2));
+        poseidon2 = await C2.deploy({
+            data: poseidonGenContract.createCode(2)
+        }).send({
+            gas: 5000000,
+            from: accounts[0]
+        });
+        
+        C4 = new web3.eth.Contract(poseidonGenContract.generateABI(4));
+        poseidon4 = await C4.deploy({
+            data: poseidonGenContract.createCode(4)
+        }).send({
+            gas: 5000000,
+            from: accounts[0]
+        });
+
+        C6 = new web3.eth.Contract(poseidonGenContract.generateABI(6));
+        poseidon6 = await C6.deploy({
+            data: poseidonGenContract.createCode(6)
+        }).send({
+            gas: 6000000,
+            from: accounts[0]
+        });
+    });
+
+    it("Should calculate the poseidon correctly for 2 inputs", async () => {
+        const res = await poseidon2.methods.poseidon([1, 2]).call();
+
+        // console.log("Cir: " + bigInt(res.toString(16)).toString(16));
+
+        const res2 = poseidon([1, 2]);
+        // console.log("Ref: " + bigInt(res2).toString(16));
+
+        assert.equal(res.toString(), res2.toString());
+    });
+    it("Should calculate the poseidon correctly for 4 inputs", async () => {
+
+        const res = await poseidon4.methods.poseidon([1, 2, 3, 4]).call();
+
+        // console.log("Cir: " + bigInt(res.toString(16)).toString(16));
+
+        const res2 = poseidon([1, 2, 3, 4]);
+        // console.log("Ref: " + bigInt(res2).toString(16));
+
+        assert.equal(res.toString(), res2.toString());
+    });
+
+
+    it("Should calculate the poseidon correctly for 6 inputs", async () => {
+
+        const res = await poseidon6.methods.poseidon([1, 2, 3, 4, 0, 0]).call();
+
+        // console.log("Cir: " + bigInt(res.toString(16)).toString(16));
+
+        const res2 = poseidon([1, 2, 3, 4, 0, 0]);
+        // console.log("Ref: " + bigInt(res2).toString(16));
+
+        assert.equal(res.toString(), res2.toString());
+    });
+
+});
+
